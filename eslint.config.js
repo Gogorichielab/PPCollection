97da4e2c3a99b4aac4bfe47034f1d970425af47b
Richard Lewis <richard@gogorichie.com>
2026-02-17 19:38:55 -0600

    chore: remove .eslintrc.json and migrate ESLint configuration to eslint.config.js feat: add globals dependency to package.json

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const globals = require('globals');
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'coverage/**', '.github/**'],
+  },
+];
